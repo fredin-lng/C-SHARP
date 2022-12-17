e1de54b88b14214341131b8624bd20f9d51621eb
Alfredo Castillo <55422424+fredin-lng@users.noreply.github.com>
2022-12-17 14:25:50 -0600

    Add files via upload

---
+++ Currency Converter/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Currency_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPound = new System.Windows.Forms.Button();
+            this.btnCanadian = new System.Windows.Forms.Button();
+            this.btnMarks = new System.Windows.Forms.Button();
+            this.btnYen = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblUS = new System.Windows.Forms.Label();
+            this.lblCurrencyConverter = new System.Windows.Forms.Label();
+            this.txtCurrencyConverter = new System.Windows.Forms.TextBox();
+            this.txtUS = new System.Windows.Forms.TextBox();
+            this.picCurrency = new System.Windows.Forms.PictureBox();
+            this.picUS = new System.Windows.Forms.PictureBox();
+            this.picMoney = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCurrency)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMoney)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPound
+            // 
+            this.btnPound.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPound.Location = new System.Drawing.Point(99, 352);
+            this.btnPound.Name = "btnPound";
+            this.btnPound.Size = new System.Drawing.Size(132, 71);
+            this.btnPound.TabIndex = 2;
+            this.btnPound.Text = "Pounds";
+            this.btnPound.UseVisualStyleBackColor = true;
+            this.btnPound.Click += new System.EventHandler(this.btnPound_Click);
+            // 
+            // btnCanadian
+            // 
+            this.btnCanadian.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCanadian.Location = new System.Drawing.Point(237, 352);
+            this.btnCanadian.Name = "btnCanadian";
+            this.btnCanadian.Size = new System.Drawing.Size(132, 71);
+            this.btnCanadian.TabIndex = 3;
+            this.btnCanadian.Text = "Canada";
+            this.btnCanadian.UseVisualStyleBackColor = true;
+            this.btnCanadian.Click += new System.EventHandler(this.btnCanadian_Click);
+            // 
+            // btnMarks
+            // 
+            this.btnMarks.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMarks.Location = new System.Drawing.Point(375, 352);
+            this.btnMarks.Name = "btnMarks";
+            this.btnMarks.Size = new System.Drawing.Size(132, 71);
+            this.btnMarks.TabIndex = 4;
+            this.btnMarks.Text = "Marks";
+            this.btnMarks.UseVisualStyleBackColor = true;
+            this.btnMarks.Click += new System.EventHandler(this.btnMarks_Click);
+            // 
+            // btnYen
+            // 
+            this.btnYen.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYen.Location = new System.Drawing.Point(513, 352);
+            this.btnYen.Name = "btnYen";
+            this.btnYen.Size = new System.Drawing.Size(132, 71);
+            this.btnYen.TabIndex = 5;
+            this.btnYen.Text = "Yen";
+            this.btnYen.UseVisualStyleBackColor = true;
+            this.btnYen.Click += new System.EventHandler(this.btnYen_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(99, 429);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(546, 70);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblUS
+            // 
+            this.lblUS.AutoSize = true;
+            this.lblUS.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUS.Location = new System.Drawing.Point(345, 99);
+            this.lblUS.Name = "lblUS";
+            this.lblUS.Size = new System.Drawing.Size(64, 20);
+            this.lblUS.TabIndex = 7;
+            this.lblUS.Text = "Dollars";
+            // 
+            // lblCurrencyConverter
+            // 
+            this.lblCurrencyConverter.AutoSize = true;
+            this.lblCurrencyConverter.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrencyConverter.Location = new System.Drawing.Point(350, 254);
+            this.lblCurrencyConverter.Name = "lblCurrencyConverter";
+            this.lblCurrencyConverter.Size = new System.Drawing.Size(55, 20);
+            this.lblCurrencyConverter.TabIndex = 8;
+            this.lblCurrencyConverter.Text = "label2";
+            // 
+            // txtCurrencyConverter
+            // 
+            this.txtCurrencyConverter.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrencyConverter.Location = new System.Drawing.Point(433, 251);
+            this.txtCurrencyConverter.Name = "txtCurrencyConverter";
+            this.txtCurrencyConverter.Size = new System.Drawing.Size(212, 29);
+            this.txtCurrencyConverter.TabIndex = 9;
+            // 
+            // txtUS
+            // 
+            this.txtUS.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUS.Location = new System.Drawing.Point(436, 96);
+            this.txtUS.Name = "txtUS";
+            this.txtUS.Size = new System.Drawing.Size(212, 29);
+            this.txtUS.TabIndex = 10;
+            this.txtUS.TextChanged += new System.EventHandler(this.txtUS_TextChanged);
+            // 
+            // picCurrency
+            // 
+            this.picCurrency.Image = global::Currency_Converter.Properties.Resources.MONEY;
+            this.picCurrency.Location = new System.Drawing.Point(99, 213);
+            this.picCurrency.Name = "picCurrency";
+            this.picCurrency.Size = new System.Drawing.Size(168, 101);
+            this.picCurrency.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCurrency.TabIndex = 1;
+            this.picCurrency.TabStop = false;
+            // 
+            // picUS
+            // 
+            this.picUS.Image = global::Currency_Converter.Properties.Resources.usa;
+            this.picUS.Location = new System.Drawing.Point(99, 55);
+            this.picUS.Name = "picUS";
+            this.picUS.Size = new System.Drawing.Size(162, 101);
+            this.picUS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUS.TabIndex = 0;
+            this.picUS.TabStop = false;
+            // 
+            // picMoney
+            // 
+            this.picMoney.Image = global::Currency_Converter.Properties.Resources.MONEY;
+            this.picMoney.Location = new System.Drawing.Point(12, 12);
+            this.picMoney.Name = "picMoney";
+            this.picMoney.Size = new System.Drawing.Size(81, 56);
+            this.picMoney.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMoney.TabIndex = 11;
+            this.picMoney.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 519);
+            this.Controls.Add(this.picMoney);
+            this.Controls.Add(this.txtUS);
+            this.Controls.Add(this.txtCurrencyConverter);
+            this.Controls.Add(this.lblCurrencyConverter);
+            this.Controls.Add(this.lblUS);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnYen);
+            this.Controls.Add(this.btnMarks);
+            this.Controls.Add(this.btnCanadian);
+            this.Controls.Add(this.btnPound);
+            this.Controls.Add(this.picCurrency);
+            this.Controls.Add(this.picUS);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCurrency)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMoney)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picUS;
+        private System.Windows.Forms.PictureBox picCurrency;
+        private System.Windows.Forms.Button btnPound;
+        private System.Windows.Forms.Button btnCanadian;
+        private System.Windows.Forms.Button btnMarks;
+        private System.Windows.Forms.Button btnYen;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblUS;
+        private System.Windows.Forms.Label lblCurrencyConverter;
+        private System.Windows.Forms.TextBox txtCurrencyConverter;
+        private System.Windows.Forms.TextBox txtUS;
+        private System.Windows.Forms.PictureBox picMoney;
+    }
+}
+
---
+++ Currency Converter/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+/// <summary>
+/// Programmer: Alfredo Castillo
+/// Program Objectinve: This program convert us currency to a different countrys currency. Only for Mark, Yen, Canadian, Pounds. 
+/// Date: 09/20/2021
+/// </summary>
+
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        ///  specify global parameters
+        /// </summary>
+        bool isNumeric;
+        double acValue;
+        double currency;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /// disable buttons on application start up
+            btnCanadian.Enabled = false;
+            btnMarks.Enabled = false;
+            btnMarks.Enabled = false;
+            btnPound.Enabled = false;
+            btnYen.Enabled = false;
+        }
+
+        private void btnPound_Click(object sender, EventArgs e)
+        {
+            ///Globalization library you can use culture
+            ///information to use the format the currency for different
+            ///countries.
+            CultureInfo culInfo = new CultureInfo("en-GB");
+
+            /// try to convert to a numeric value
+            isNumeric = double.TryParse(txtUS.Text, out acValue);
+
+            /// if numeric value, convert currency amount to yen. else clear us text box and converted text box.
+            if (isNumeric)
+            {                
+                currency = Convert.ToDouble(txtUS.Text) * 0.73;
+
+                txtCurrencyConverter.Text = currency.ToString("C", culInfo);
+
+                lblCurrencyConverter.Text = "Pounds";
+                picCurrency.Image = Properties.Resources.uk;
+            }
+            else
+            {
+                reset();
+            }
+        }
+
+        private void btnCanadian_Click(object sender, EventArgs e)
+        {
+            ///Globalization library you can use culture
+            ///information to use the format the currency for different
+            ///countries.
+            CultureInfo culInfo = new CultureInfo("en-GB");
+
+            /// try to convert to a numeric value
+            isNumeric = double.TryParse(txtUS.Text, out acValue);
+
+            /// if numeric value, convert currency amount to yen. else clear us text box and converted text box.
+            if (isNumeric)
+            {
+                currency = Convert.ToDouble(txtUS.Text) * 1.28;
+
+                txtCurrencyConverter.Text = currency.ToString("C", culInfo);
+
+                lblCurrencyConverter.Text = "Canadian";
+                picCurrency.Image = Properties.Resources.canada;
+            }
+            else
+            {
+                reset();
+            }
+        }
+
+        private void btnMarks_Click(object sender, EventArgs e)
+        {
+            ///Globalization library you can use culture
+            ///information to use the format the currency for different
+            ///countries.
+            CultureInfo culInfo = new CultureInfo("en-GB");
+
+            /// try to convert to a numeric value
+            isNumeric = double.TryParse(txtUS.Text, out acValue);
+
+            /// if numeric value, convert currency amount to yen. else clear us text box and converted text box.
+            if (isNumeric)
+            {
+                currency = Convert.ToDouble(txtUS.Text) * 1.66712;
+
+                txtCurrencyConverter.Text = currency.ToString("C", culInfo);
+
+                lblCurrencyConverter.Text = "DEM";
+                picCurrency.Image = Properties.Resources.germany;
+            }
+            else
+            {
+                reset();
+            }
+        }
+
+        private void btnYen_Click(object sender, EventArgs e)
+        {
+            ///Globalization library you can use culture
+            ///information to use the format the currency for different
+            ///countries.
+            CultureInfo culInfo = new CultureInfo("en-GB");
+            
+            /// try to convert to a numeric value
+            isNumeric = double.TryParse(txtUS.Text, out acValue);
+
+            /// if numeric value, convert currency amount to yen. else clear us text box and converted text box.
+            if (isNumeric)
+            {
+                currency = Convert.ToDouble(txtUS.Text) * 109.88;
+                txtCurrencyConverter.Text = currency.ToString("C", culInfo);
+
+                lblCurrencyConverter.Text = "Yen";
+                picCurrency.Image = Properties.Resources.japan;
+            }
+            else
+            {
+                reset();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            /// clears out the all the textboxes and the converted flag image
+            txtUS.Text = "";
+            txtCurrencyConverter.Text = "";
+            picCurrency.Image = Properties.Resources.MONEY;
+            txtUS.Select();
+        }
+
+        private void reset()
+        {
+            /// clears out the all the textboxes and the currency converted flag image
+            //MessageBox.Show("Only numbers allowed!", " Input Error");
+            txtUS.Text = "";
+            txtCurrencyConverter.Text = "";
+            picCurrency.Image = Properties.Resources.MONEY;
+            txtUS.Select();
+        }
+
+        public bool isUsCurrencyEmpty()
+        {
+            /// check if us currency text box is empty and return true if empty, false if not empty
+            if (String.IsNullOrWhiteSpace(txtUS.Text) || txtUS.Text.Trim().Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void txtUS_TextChanged(object sender, EventArgs e)
+        {
+            
+            /// enables buttons if us currency text box is not empty
+            if (isUsCurrencyEmpty() == false)
+            {
+                btnCanadian.Enabled = true;
+                btnMarks.Enabled = true;
+                btnMarks.Enabled = true;
+                btnPound.Enabled = true;
+                btnYen.Enabled = true;
+            }
+            /// enables buttons if us currency text box is not empty
+            else
+            {
+                btnCanadian.Enabled = false;
+                btnMarks.Enabled = false;
+                btnMarks.Enabled = false;
+                btnPound.Enabled = false;
+                btnYen.Enabled = false;
+            }
+            isNumeric = double.TryParse(txtUS.Text, out acValue);
+
+            /// displays error if a character other than a numberic is entered
+            if (!isNumeric && txtUS.Text != "")
+            {
+                MessageBox.Show("Only Numbers allowed", "Invalid Input");
+                txtUS.Text = "";
+            }
+        }        
+    }
+}
